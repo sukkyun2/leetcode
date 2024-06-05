f55a8ec4ca8be22363654e93a9a780282375a4a7
suk kyun hong <38815540+sukkyun2@users.noreply.github.com>
2024-06-05 17:23:24 +0900

    Time: 415 ms (31.52%) | Memory: 17.8 MB (21.30%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,16 +1,17 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        visited = [[False for _ in range(n)] for _ in range(m)]
+        # visited = [[False for _ in range(n)] for _ in range(m)]
 
         def dfs(i, j):
             if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] == 0:
                 return 1
 
-            if visited[i][j]:
+            # if visited[i][j]:
+            if grid[i][j] == -1:
                 return 0
-                
-            visited[i][j] = True
+
+            grid[i][j] = -1
             
             return dfs(i+1, j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j-1) 
 
