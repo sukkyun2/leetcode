cce19be367085b4b231bd602d95b5cadb8365b35
suk kyun hong <38815540+sukkyun2@users.noreply.github.com>
2024-06-05 16:49:55 +0900

    Time: 68 ms (48.56%) | Memory: 16.6 MB (71.39%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -3,15 +3,15 @@ from collections import deque
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         m,n = len(image), len(image[0])
-        visited = [[False for _ in range(n)] for _ in range(m)]
 
         def outOfIndex(x,y):
             return x < 0 or y < 0 or x >= m or y >= n
 
         def bfs():
             og = image[sr][sc]
+            if og == color: return
+
             q = deque([(sr,sc)])
-            visited[sr][sc] = True
             image[sr][sc] = color 
 
             while q:
@@ -19,8 +19,7 @@ class Solution:
                 
                 for x,y in [(1,0),(-1,0),(0,1),(0,-1)]:
                     new_x, new_y = i + x, j + y
-                    if not outOfIndex(new_x, new_y) and not visited[new_x][new_y] and image[new_x][new_y] == og:
-                        visited[new_x][new_y] = True
+                    if not outOfIndex(new_x, new_y) and image[new_x][new_y] == og:
                         image[new_x][new_y] = color
                         q.append((new_x, new_y))
     
