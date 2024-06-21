376f69ad5e4e6a814946c39de328061afacf3a33
suk kyun hong <38815540+sukkyun2@users.noreply.github.com>
2024-06-21 11:35:07 +0900

    Time: 39 ms (36.51%) | Memory: 16.6 MB (79.31%) - LeetSync

---
+++ 399-evaluate-division/evaluate-division.py
@@ -0,0 +1,31 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        def conv_adj():
+            adj = defaultdict(list)
+            for (start, end), value in zip(equations, values):
+                adj[start].append((end, value))
+                adj[end].append((start, 1 / value))
+            return adj
+
+        def dfs(end, curr, curr_val, visited):
+            visited[curr] = True
+            if curr == end:
+                return curr_val
+
+            for u, v in adj[curr]:
+                if not visited[u]:
+                    ans = dfs(end, u, v, visited)
+                    if ans != -1.0:
+                        return ans * curr_val
+
+            return -1.0
+
+        adj = conv_adj()
+        ans = []
+        for start, end in queries:
+            if end not in adj.keys() or not adj[end]:
+                ans.append(-1.0)
+            else:
+                ans.append(dfs(end, start, 1, defaultdict(bool)))
+
+        return ans
