f9d85d684661a56e1405083ee9b70804924fcf26
suk kyun hong <38815540+sukkyun2@users.noreply.github.com>
2024-07-06 11:33:50 +0900

    Time: 2108 ms (10.08%) | Memory: 18.3 MB (52.75%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -1,31 +1,35 @@
 from collections import deque
 
 class Solution:
-    def minimumEffortPath(self, h: List[List[int]]) -> int:
-        m,n = len(h), len(h[0])
-        efforts = {(i,j):float('inf') for j in range(n) for i in range(m)}
-        efforts[(0,0)] = 0
-        q = deque([(0,0)])
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        H = len(heights)
+        W = len(heights[0])
 
-        def out_of_index(i,j):
-            return i < 0 or i >= m or j < 0 or j >= n
+        queue = deque([])
+        queue.append((0, 0))
 
-        while q:
-            i, j = q.popleft()
+        cost = { (row, col): float('inf') for col in range(W) for row in range(H) }
+        cost[(0, 0)] = 0
 
-            # if i == m-1 and j == n-1:
-            #     print(efforts)
-            #     return efforts[(i,j)]
-            
-            for x, y in [(1,0),(-1,0),(0,-1),(0,1)]:
-                new_x, new_y = i+x, j+y
-                if out_of_index(new_x, new_y):
-                    continue
-                
-                effort = max(efforts[(i,j)], abs(h[i][j]-h[new_x][new_y]))
-                if effort < efforts[(new_x,new_y)]:
-                    efforts[(new_x,new_y)] = effort
-                    q.append((new_x,new_y))
+        directions = [[1,0], [0,1], [-1,0], [0,-1]]
+        
+        def inside(row, col):
+            return 0 <= row < H and 0 <= col < W
+        
+        while queue:
+            row, col = queue.popleft()
+            current_height = heights[row][col]
+            current_cost = cost[(row, col)]
 
+            for d_row, d_col in directions:
+                new_row = row + d_row
+                new_col = col + d_col
 
-        return efforts[(m-1,n-1)]
+                if inside(new_row, new_col):
+                    neighbor_height = heights[new_row][new_col]
+                    new_cost = max(current_cost, abs(neighbor_height - current_height))
+                    if new_cost < cost[(new_row, new_col)]:
+                        cost[(new_row, new_col)] = new_cost
+                        queue.append((new_row, new_col))
+        
+        return cost[(H - 1, W - 1)]
