3918ee1dcec15211777e52a89a8e7a37401b28be
suk kyun hong <38815540+sukkyun2@users.noreply.github.com>
2024-07-15 18:48:33 +0900

    Time: 358 ms (23.30%) | Memory: 23.3 MB (41.55%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class Solution:
+    def numRescueBoats(self, p: List[int], limit: int) -> int:
+        p.sort()
+        ans, front, rear = 0, 0, len(p) - 1
+
+        while front < rear:
+            tot = p[front] + p[rear]
+            if tot > limit:
+                rear -= 1
+                ans += 1
+            elif tot <= limit:
+                front += 1
+                rear -= 1
+                ans += 1   
+
+
+
+        return ans + (front == rear)
+        
+        
