aee2de44c0f2abb140a4bee7f42357e0ce83976d
suk kyun hong <38815540+sukkyun2@users.noreply.github.com>
2024-07-23 21:29:03 +0900

    Time: 143 ms (9.25%) | Memory: 17.8 MB (40.43%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -1,14 +1,10 @@
 class Solution:
     def largestOddNumber(self, num: str) -> str:
-        ans = ""
         last=-1
 
         for i, c_num in enumerate(num):
             n = int(c_num)
-
-            if n%2 == 0:
-                continue
-            
+            if n%2 == 0: continue
             last=i
 
         return num[:last+1]
