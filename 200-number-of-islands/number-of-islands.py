7679be08c52f85121580e26fcf1213070206f73a
sukkyun hong <38815540+sukkyun2@users.noreply.github.com>
2024-09-11 12:19:58 +0900

    Time: 276 ms (26.63%) | Memory: 24.2 MB (33.15%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -3,26 +3,28 @@ class Solution:
         m,n = len(grid), len(grid[0])
         directions = [(1,0),(0,1),(-1,0),(0,-1)]
         visited = set()
-        ans = 0
+        ans = 0 
 
         def out_of_index(i,j):
-            return i>=m or j>=n or i<0 or j<0 
+            return i<0 or j<0 or i>=m or j>=n
 
         def dfs(i,j,visited):
-            visited.add((i,j))
-
-            for x,y in directions:
-                new_x, new_y = i+x, j+y
-                if out_of_index(new_x,new_y) or grid[new_x][new_y] == "0" or (new_x,new_y) in visited:
-                    continue
+            stack = [(i,j)]
+            
+            while stack:
+                ii, jj = stack.pop()
+                visited.add((ii,jj))
                 
-                dfs(new_x,new_y,visited)
+                for x,y in directions:
+                    new_x,new_y = ii+x, jj+y
+                    if out_of_index(new_x,new_y) or (new_x,new_y) in visited or grid[new_x][new_y] == "0":
+                        continue
+                    stack.append((new_x,new_y))
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == "1" and (i,j) not in visited:
                     dfs(i,j,visited)
                     ans+=1
-        print(len(visited))
+
         return ans
-        
