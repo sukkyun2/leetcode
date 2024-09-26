dafb7146f1ac2ec4126d31e91a3f6d272d1e5ec4
sukkyun hong <38815540+sukkyun2@users.noreply.github.com>
2024-09-26 21:37:47 +0900

    Time: 49 ms (23.45%) | Memory: 16.7 MB (12.63%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -1,27 +1,32 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        n = len(costs) // 2
-        cal = sorted(map(lambda cost: (cost[0]-cost[1],cost), costs), key=lambda c: abs(c[0]), reverse=True)
-        
-        A, B = [], []
+        people_max = len(costs)//2
+        ans = 0
+        count_a, count_b = 0,0
+        costs.sort(key=lambda x: abs(x[0]-x[1]), reverse=True)
+        print(costs)
 
-        i = 0
-        while i < len(cal) and len(A) < n and len(B) < n:
-            diff, cost = cal[i]
-            if diff < 0:
-                A.append(cost[0])
+        i=0
+        while i < len(costs) and count_a < people_max and count_b < people_max:
+            a,b = costs[i]
+            if a < b:
+                print(a)
+                count_a += 1
+                ans += a
             else:
-                B.append(cost[1])
-            i+=1
+                print(b)
+                count_b += 1
+                ans += b
+            i+=1 
         
-        while len(B) < n:
-            diff, cost = cal[i]
-            B.append(cost[1])
+        while count_a != people_max:
+            count_a += 1
+            ans += costs[i][0]
+            i+=1 
+        while count_b != people_max:
+            count_b += 1
+            ans += costs[i][1]
             i+=1
+
+        return ans
         
-        while len(A) < n:
-            diff, cost = cal[i]
-            A.append(cost[0])
-            i+=1            
-                
-        return sum(A) + sum(B)
