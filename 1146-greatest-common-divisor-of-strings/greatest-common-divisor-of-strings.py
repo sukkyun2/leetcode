efb6088a3d9a585cf323a2c5a8729b6e2fe88b72
sukkyun hong <38815540+sukkyun2@users.noreply.github.com>
2024-09-30 14:22:42 +0900

    Time: 42 ms (22.93%) | Memory: 16.6 MB (17.87%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,11 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if not str2:
+            return str1
+        if len(str1) < len(str2):
+            return self.gcdOfStrings(str2, str1)
+        
+        if str1[:len(str2)] == str2:
+            return self.gcdOfStrings(str1[len(str2):],str2)
+        else:
+            return ""
