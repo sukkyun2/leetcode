f829041a2a6da0b4feb9d881aa605e515b54ee9a
sukkyun hong <38815540+sukkyun2@users.noreply.github.com>
2024-10-28 14:28:47 +0900

    Time: 0 ms (100.00%) | Memory: 40 MB (90.75%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    private int[] memo; 
+    public int tribonacci(int n) {
+        if(n==0) return 0;
+        if(n==1||n==2) return 1;
+
+        memo = new int[n+1];
+
+        memo[0] = 0;
+        memo[1] = 1;
+        memo[2] = 1;
+
+        for(int i=3;i<=n;i++){
+            memo[i] = memo[i-3] + memo[i-2] + memo[i-1];
+        }
+
+        return memo[n];
+    }
+}
