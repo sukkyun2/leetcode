4518787e10a9c7e5519626c0303d8e051a1604dd
sukkyun hong <38815540+sukkyun2@users.noreply.github.com>
2024-10-29 13:14:53 +0900

    Time: 1 ms (83.67%) | Memory: 41.3 MB (92.78%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,23 +1,25 @@
+import java.util.*;
+
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String ans = "";
+        StringBuilder sb = new StringBuilder();
 
         int i=0;
 
         while(i < word1.length() && i < word2.length()){
-            ans += String.valueOf(word1.charAt(i));
-            ans += String.valueOf(word2.charAt(i));
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));
             i++;
         }
 
         for(int j=i;j<word1.length();j++){
-            ans += String.valueOf(word1.charAt(j));
+            sb.append(word1.charAt(j));
         }
 
         for(int j=i;j<word2.length();j++){
-            ans += String.valueOf(word2.charAt(j));
+            sb.append(word2.charAt(j));
         }
 
-        return ans;
+        return sb.toString();
     }
 }
